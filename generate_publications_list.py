5f459ec9f3c92f1092fcd61119011d3d8ad32bd4
Andrew Chalmers <andrewchalmers26@gmail.com>
2024-02-03 02:28:30 +1300

    refactored publications

--- generate_publications_list.py
+++ generate_publications_list.py
@@ -3,14 +3,38 @@ from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import convert_to_unicode
 from bibtexparser.bibdatabase import BibDatabase
 
+import os
 import sys
 import re
+import math
+
+
+additional_info_map = 	{	'chalmers2023real':[	['DOI', 'https://dl.acm.org/doi/10.1145/3610539.3630250'],
+													['Event Page', 'https://asia.siggraph.org/2023/presentation/?id=real_110&sess=sess210'],
+													['Video', 'https://youtu.be/lcLjtdlc-7k?feature=shared']],
+							'rhee2023real':[		['DOI', 'https://dl.acm.org/doi/10.1145/3588430.3597245'],
+													['Event Page', 'https://s2023.siggraph.org/presentation/?id=real_106&sess=sess258'], 
+													['Video', 'https://youtu.be/OvC_Wwo6LXU?feature=shared'], 
+													['Audience Choice Award', '']],
+							'rhee2022teleport':[	['DOI', 'https://dl.acm.org/doi/10.1145/3550453.3570123'],
+													['Event Page', 'https://sa2022.siggraph.org/en/presentation/?id=real_107&sess=sess143'],
+													['Video', 'https://youtu.be/WoJE1uS8SAo?feature=shared']],
+							'suppan2021neural':[	['Best Conference Paper Award', '']],
+							'rhee2018mr360':[		['Second Place Award', '']],
+							'thompson2019real-caustics':[	['Poster', '']]
+						}
+
+
+def get_file_size(file_path):
+	if os.path.exists(file_path):
+		fileSize = os.path.getsize(file_path) / (1024 * 1024)
+		return str(math.ceil(fileSize))+"MB"
+	return None
 
 def parse_bib_file(file_path):
 	bibtex_str = ""
 	with open(file_path, 'r', encoding='utf-8') as file:
 		bibtex_str = file.read()
-
 
 	entries = []
 	current_entry = None
@@ -46,11 +70,201 @@ def parse_bib_file(file_path):
 def convert_to_html_list(entries):
 	entries.sort(key=lambda x: x.get('year', 0), reverse=True)
 
-	html_list = ""
+	html_list = """
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ANDREW CHALMERS</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow&display=swap">
+	<style>
+		/* Add some basic CSS for layout */
+		body {
+			font-family: Arial, sans-serif;
+			margin: 0;
+			padding: 0;
+			background-color: #fff; /* Set background color to white for the entire body */
+		}
+		.container {
+			max-width: 800px;
+			margin: 0 auto; /* Center the container */
+			padding: 20px; /* Add margin around the container */
+			background-color: #fff; /* Add a white background to the container */
+			box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1); /* Add a box shadow for a card effect */
+		}
+		header {
+			background-color: #fff; /* Add white background to the header */
+			color: #333; /* Change text color for better contrast */
+			padding: 10px;
+			display: flex;
+			justify-content: space-between;
+			align-items: center; /* Vertically center content */
+			border-bottom: 1px solid #333; /* Add a horizontal line */
+			font-family: 'PT Sans Narrow', sans-serif; /* Apply PT Sans Narrow to the header */
+		}
+		header h1 {
+			margin: 0;
+			font-family: 'PT Sans Narrow', sans-serif; /* Change the font family */
+		}
+		nav ul {
+			list-style-type: none;
+			padding: 0;
+			margin: 0;
+		}
+		nav ul li {
+			display: inline;
+			margin-right: 20px;
+			padding-right: 20px;
+		}
+		nav ul li:last-child {
+			margin-right: 0;
+			padding-right: 0;
+		}
+		/* Change tab text color to black and make it bold */
+		nav ul li a {
+			color: #000;
+			font-weight: normal;
+			text-decoration: none; /* Remove underlines */
+		}
+		/* Style for active tab */
+		nav ul li.active a {
+			font-weight: bold;
+		}
+		main .content {
+			display: block; /* Single-column layout */
+			padding-right: 20px;
+		}
+		main .content .text {
+			margin-top: 20px; /* Add margin to create space between text and header line */
+		}
+		img {
+			max-width: 100%;
+			height: auto;
+		}
+		h2 {
+			text-align: center; /* Centered "Research" text */
+		}
 
+		paper_title {
+			font-weight: bold;
+			color: #2980b9;
+			font-size: 14px;
+		}
+
+		paper_authors {
+			font-size: 12px;
+		}
+
+		paper_publisher {
+			font-size: 12px;
+		}
+
+		
+
+		/* Style for the list */
+		ul {
+			list-style: none;
+			padding: 0;
+			margin: 0;
+		}
+
+		/* Style for each list item */
+		li {
+			margin-bottom: 20px;
+			overflow: hidden;
+			display: flex;
+			align-items: center;
+		}
+
+		/* Style for thumbnail image */
+		img {
+			width: 100px;
+			height: auto;
+			margin-right: 10px;
+			border: 1px solid black;
+			padding: 5px;
+			border-radius: 5px;
+		}
+
+		/* Style for text */
+		.item-text {
+			margin-top: 0;
+		}
+
+		/* Style for the button */
+		.button-main {
+		  padding: 1px 5px;
+		  font-size: 12px;
+		  background-color: #3498db; 
+		  color: #fff; 
+		  border: none;
+		  border-radius: 5px;
+		  cursor: pointer;
+		}
+
+		/* Hover effect */
+		.button-main:hover {
+		  background-color: #2980b9; 
+		}
+
+		.button-video {
+		  padding: 1px 5px;
+		  font-size: 12px;
+		  background-color: #ff0000; 
+		  color: #fff; 
+		  border: none;
+		  border-radius: 5px;
+		  cursor: pointer;
+		}
+
+		.button-video:hover {
+		  background-color: #cc0000; 
+		}
+
+		.button-award {
+		  padding: 1px 5px;
+		  font-size: 12px;
+		  background-color: #cca300; 
+		  color: #fff; 
+		  border: none;
+		  border-radius: 5px;
+		  cursor: pointer;
+		}
+
+		/* Hover effect */
+		.button-award:hover {
+			background-color: #cca300;
+			cursor: auto; /* Explicitly set cursor to default arrow */
+		}
+
+	</style>
+</head>
+<body>
+	<div class="container"> <!-- Added a container div for centering -->
+		<header>
+			<h1>ANDREW CHALMERS</h1>
+			<nav>
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li class="active"><a href="research.html"><b>Research</b></a></li>
+				</ul>
+			</nav>
+		</header>
+		<!-- Add a horizontal line below the header -->
+		<hr style="border: none; border-top: 1px solid #333; margin: 0;">
+
+		<main>
+			<h2>Research</h2> <!-- Centered "Research" text with Roboto font -->
+			<div class="content">
+				<!-- Add your research content here -->
+				<!-- Add any research-related images here -->
+				<img src="images/research-banner.png" alt="Research Banner" style="width: 100%;">
+				<h2>Publications</h2>
+"""
+
+	# TODO BIBTEX copy/paste, URL links like YouTube
 	current_year = None
 	for entry in entries:
-		#print(entry)
+		entry_key = entry.get('entry_key', 'Unknown')
 		year = entry.get('year', 'Unknown')
 		if year != current_year:
 			if current_year is not None:
@@ -59,26 +273,121 @@ def convert_to_html_list(entries):
 			html_list += "        <ul>\n"
 			current_year = year
 
-		html_list += "          <li>\n"
-		html_list += f"            <img class=\"thumbnail\" src=\"{entry.get('thumbnail_src', './data/dummy_thumb.png')}\" alt=\"{entry.get('thumbnail_alt', 'Publication Thumbnail')}\">\n"
-		html_list += "            <div>\n"
-		html_list += f"                <paper_title>{entry.get('title', 'Unknown Title')}</paper_title><br>\n"
-		html_list += f"                <paper_authors>{entry.get('author', 'Unknown Authors')}</paper_authors><br>\n"
-		html_list += f"                <paper_publisher>{entry.get('booktitle', 'Unknown booktitle')}</paper_publisher><br>\n"
-		html_list += "            </div>\n"
-		html_list += "          </li>\n"
-	
+		title = entry.get('title', 'Unknown Title')
+		publisher = refine_html_journal(entry.get('booktitle', entry.get('journal', 'Misc.')))
+
+		# Special case, paper has same ID as the poster
+		if title=='Real-time underwater caustics for mixed reality 360° videos':
+			entry_key = entry_key+'-caustics'
+			publisher += ' (Poster)'
+
+		link_thumbnail = './thumbnails/'+entry_key+'-thumbnail.jpg'
+
+		link_pdf = './papers/'+entry_key+'.pdf'
+		link_pdf_compressed = './papers-compressed/'+entry_key+'-compressed.pdf'
+		link_pdf_appendix = './papers/'+entry_key+'-appendix.pdf'
+		if not os.path.exists(link_pdf_appendix):
+			link_pdf_appendix = None
+
+
+		additional_info = []
+		if entry_key in additional_info_map:
+			additional_info = additional_info_map[entry_key]
+		
+		#print(year, entry.get('title'))
+		if publisher!='Misc.':
+			html_list += "          <li>\n"
+			html_list += f"            <img src=\"{link_thumbnail}\" alt=\"{entry.get('thumbnail_alt', 'Thumbnail')}\">\n"
+			html_list += "            <div class=\"item-text\">\n"
+			html_list += f"                <paper_title>{title}</paper_title><br>\n"
+			html_list += f"                <paper_authors>{entry.get('author', 'Unknown Authors')}</paper_authors><br>\n"
+			html_list += f"                <paper_publisher>{publisher}, {year}</paper_publisher><br>\n"
+			html_list += f"                <button class=\"button-main\" onclick=\"window.open(\'{link_pdf}\', \'_blank\')\">PDF {get_file_size(link_pdf)}</button>"
+			if get_file_size(link_pdf_compressed)!=None and get_file_size(link_pdf_compressed)!="1MB":
+				html_list += f"                <button class=\"button-main\" onclick=\"window.open(\'{link_pdf_compressed}\', \'_blank\')\">PDF {get_file_size(link_pdf_compressed)}</button>\n"
+			if link_pdf_appendix:
+				html_list += f"                <button class=\"button-main\" onclick=\"window.open(\'{link_pdf_appendix}\', \'_blank\')\">Appendix {get_file_size(link_pdf_appendix)}</button>\n"				
+			for item in additional_info:
+				button_style = "button-main"
+				if item[0]=='Video':
+					button_style = "button-video"
+				elif item[0]=='Poster':
+					item[1] = './papers/'+entry_key+'-poster.pdf'
+				elif 'Award' in item[0]:
+					button_style = "button-award"
+
+				if item[1]=='':
+					html_list += f"                <button class=\"{button_style}\"\">{item[0]}</button>\n"
+				else:
+					html_list += f"                <button class=\"{button_style}\" onclick=\"window.open(\'{item[1]}\', \'_blank\')\">{item[0]}</button>\n"
+			
+			html_list += "            </div>\n"
+			html_list += "          </li>\n"
 	html_list += "        </ul>\n"
+
+	html_list = refine_html(html_list)
+
+	html_list +="""
+			</main>
+		</div>
+
+	</body>
+</html>
+"""
 
 	return html_list
 
+def refine_html(html_list):
+	html_list = html_list.replace("Chalmers, Andrew", "<b>Chalmers, Andrew</b>")
+	html_list = html_list.replace("\\&", "&")
+	html_list = html_list.replace("Rhee, Tae Hyun", "Rhee, Taehyun")
+	html_list = html_list.replace("Pacific Graphics Short Papers, Posters, and Work-in-Progress Papers", "Pacific Graphics (Short Papers)")
+	html_list = html_list.replace("PG (Short Papers)", "Pacific Graphics")
+	html_list = html_list.replace("PG", "Pacific Graphics")
+	html_list = html_list.replace("and others", "and Rhee, Taehyun")
+	html_list = html_list.replace("{\\\"", "")
+
+	return html_list
+
+def refine_html_journal(journal_info):
+	# Define a regular expression pattern to match four consecutive digits
+	digit_pattern = re.compile(r'\d{4}')
+
+	# Find all matches of four consecutive digits in the input string
+	digit_matches = digit_pattern.findall(journal_info)
+
+	# Remove the four consecutive digits from the input string
+	output_string = re.sub(digit_pattern, '', journal_info)
+
+	# Define a regular expression pattern to match text within brackets ()
+	bracket_pattern = re.compile(r'\([^)]*\)')
+
+	# Remove text within brackets from the output string
+	output_string = re.sub(bracket_pattern, '', output_string)
+
+	# Remove any double white spaces
+	output_string = re.sub(r'\s+', ' ', output_string)
+
+	# Capitalize the first letter of each word except for connecting words
+	connecting_words = ["on", "the", "and"]
+	words = output_string.split()
+	updated_words = [word if word.isupper() else word.capitalize() if word.lower() not in connecting_words else word for word in words]
+
+	# Join the words back into a string
+	output_string = ' '.join(updated_words)
+
+	return output_string.strip()
+
+
 if __name__ == "__main__":
-	bib_file_path = "./data/citations.bib"  # Replace with your BibTeX file path
-	entries = parse_bib_file(bib_file_path)
+	bib_file_path = "citations.bib" 
+	bib_file_path_additional = "citations_additional.bib" 
+
+	entries = parse_bib_file(bib_file_path)+parse_bib_file(bib_file_path_additional)
 
 	html_list = convert_to_html_list(entries)
 
-	with open("output.html", "w", encoding="utf-8") as output_file:
+	with open("research.html", "w", encoding="utf-8") as output_file:
 		output_file.write(html_list)
 
 	print("Conversion complete. HTML list saved to output.html")
---
+++ generate_thumbs.py
@@ -0,0 +1,196 @@
+import glob
+import fitz
+
+import cv2
+import numpy as np
+import sys
+import os
+
+# crop_params = top left corner, and size, expressed as ratio
+
+cases = {	'chalmers2024avatar360':(0,((0.2,0.6),(0.35,0.35))),
+			'chen2024neural':(4,((0.15,0.05),(0.45,0.8))),
+			'rhee2023real':(0,((0.3,0.42),(0.34,0.34))),
+			'chalmers2023real':(0,((0.36,0.7),(0.1,0.1))),
+			'zaman2023mrmac':(0,((0.15,0.31),(0.25,0.25))),
+			'chalmers2023motion':(0,((0.45,0.59),(0.15,0.15))),
+			'zhao2023deep':(0,((0.26,0.57),(0.2,0.2))),
+			'o2023simulating':(0,((0.16,0.5),(0.3,0.3))),
+			'weir2023lighting':(0,((0.25,0.5),(0.54,0.54))),
+			'chalmers2022illumination':(1,((0.08,0.15),(0.4,0.7))),
+			'chen2022casual':(1,((0.05,0.05),(0.17,0.25))),
+			'weir2022deep':(1,((0.15,0.345),(0.2,0.2))),
+			'rhee2022teleport':(0,((0.34,0.61),(0.15,0.18))),
+			'suppan2021neural':(1,((0.11,0.7),(0.18,0.18))),
+			'chalmers2020reconstructing':(1,((0.05,0.8),(0.13,0.13))),
+			'chalmers2020illumination':(0,((0.3,0.05),(0.28,0.9))),
+			'thompson2019real':(0,((0.15,0.5),(0.26,0.5))),
+			'thompson2019underwater':(1,((0.05,0.05),(0.3,0.45))),
+			'thompson2019real-caustics':(0,((0.2,0.54),(0.27,0.4))),
+			'chalmers2018illumination':(32,((0.43,0.2),(0.5,0.9)))
+		}
+
+def remove_whitespace(image_array):
+    # Find indices of non-white pixels along rows and columns
+    non_white_rows = np.any(image_array != 255, axis=1)
+    non_white_cols = np.any(image_array != 255, axis=0)
+
+    # Find the bounding box of the non-white region
+    min_row, max_row = np.where(non_white_rows)[0][[0, -1]]
+    min_col, max_col = np.where(non_white_cols)[0][[0, -1]]
+
+    # Crop the image_array based on the bounding box
+    cropped_image_array = image_array[min_row:max_row+1, min_col:max_col+1]
+
+    return cropped_image_array
+
+def crop_image_by_ratio(image, top_left_ratio, size_ratio, do_remove_whitespace=True):
+    height, width, _ = image.shape
+
+    y_ratio, x_ratio = top_left_ratio
+    h_ratio, w_ratio = size_ratio
+
+    y = int(y_ratio * height)
+    x = int(x_ratio * width)
+    h = int(h_ratio * width) # instead of height, do it relative to width
+    w = int(w_ratio * width)
+
+    cropped_image = image[y:y+h, x:x+w, :]
+
+    if do_remove_whitespace:
+	    cropped_image = remove_whitespace(cropped_image)
+
+    return cropped_image
+
+def applyBorder(img, T=5):
+	img[:,:T,:] = 0
+	img[:,img.shape[1]-1-T:img.shape[1]-1,:] = 0
+	img[:T,:,:] = 0
+	img[img.shape[1]-1-T:img.shape[1]-1,:,:] = 0
+	return img
+
+def extract_and_save_page_as_image(pdf_path, page_number, crop_params, base_name, save_folder):
+	thumbnail_filename = f"{save_folder}/{base_name}-thumbnail.jpg"
+	target_resolution = (300,300)
+
+	# Open the PDF file
+	pdf_document = fitz.open(pdf_path)
+
+	# Check if the specified page number is valid
+	if page_number < pdf_document.page_count:
+		# Get the specified page
+		page = pdf_document[page_number]
+		pix = page.get_pixmap(dpi=200)  # render page to an image
+		img = cv2.imdecode(np.frombuffer(pix.tobytes("ppm"), np.uint8), cv2.IMREAD_COLOR)
+		if crop_params is not None:
+			img = crop_image_by_ratio(img,crop_params[0],crop_params[1])
+		img = pad_into_square(img)
+		img = cv2.resize(img, target_resolution, interpolation=cv2.INTER_AREA)
+		#img = applyBorder(img)
+
+		cv2.imwrite(thumbnail_filename, img)
+
+		# Close the PDF file
+		pdf_document.close()
+	else:
+		print(f"Invalid page number: {page_number}")
+
+def pad_into_square(image_array):
+	# Get the height and width of the image
+	height, width, _ = image_array.shape
+
+	# Determine the padding needed to make the image square
+	if height < width:
+		pad_size = width - height
+		top_pad = pad_size // 2
+		bottom_pad = pad_size - top_pad
+		padded_image = np.pad(image_array, ((top_pad, bottom_pad), (0, 0), (0, 0)), mode='constant', constant_values=255)
+	elif width < height:
+		pad_size = height - width
+		left_pad = pad_size // 2
+		right_pad = pad_size - left_pad
+		padded_image = np.pad(image_array, ((0, 0), (left_pad, right_pad), (0, 0)), mode='constant', constant_values=255)
+	else:
+		# Image is already square, no padding needed
+		padded_image = image_array.copy()
+
+	return padded_image
+
+def get_pdfs_in_subfolder(folder_path):
+	pdf_files = glob.glob(f"{folder_path}/**/*.pdf", recursive=True)
+	# Exclude files with "-compressed.pdf" in their names
+	pdf_files = [file for file in pdf_files if "-compressed.pdf" not in file]
+	return pdf_files
+
+def process_img(pdf_document, image_index, base_name, save_folder): 
+	target_resolution = (300,300)
+
+	# Get the image data
+	image_data = pdf_document.extract_image(image_index)
+	image_bytes = image_data["image"]
+
+	# Convert image data to NumPy array
+	img = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
+	img = pad_into_square(img)
+	img = cv2.resize(img, target_resolution, interpolation=cv2.INTER_AREA)
+
+	# Save the image to the "thumbnails" subfolder
+	thumbnail_filename = f"{save_folder}/{base_name}-thumbnail.jpg"
+	#print("Writing:",thumbnail_filename)
+	cv2.imwrite(thumbnail_filename, img)
+
+def generate_thumb(pdf_path):
+	# Open the PDF file
+	pdf_document = fitz.open(pdf_path)
+	base_name = os.path.splitext(os.path.basename(pdf_path))[0]
+	save_folder = 'thumbnails'
+
+	if not os.path.exists(save_folder):
+		os.makedirs(save_folder)
+
+	page_number, crop_params = 0, None
+	try:
+		page_number, crop_params = cases[base_name]
+	except:
+		pass
+
+	if crop_params is None:
+		return
+	extract_and_save_page_as_image(pdf_path, page_number, crop_params, base_name, save_folder)
+	return
+
+	## Iterate through pages
+	#for page_number in range(pdf_document.page_count):
+	#	page = pdf_document[page_number]
+	#	# Get the first image on the page
+	#	images = page.get_images(full=True)
+	#	if images:
+	#		first_image = images[img_index]
+	#		process_img(pdf_document, first_image[0], base_name, save_folder)
+	#		pdf_document.close()
+	#		return
+
+	# No images found in the PDF
+	print("No images found in the PDF:",pdf_path)
+	thumbnail_filename = f"{save_folder}/{base_name}-thumbnail.jpg"
+	#print("Writing:",thumbnail_filename)
+	cv2.imwrite(thumbnail_filename, np.ones((50,50,3))*255)
+
+def generate_thumbs():
+	print("Generating thumbs...")
+
+	subfolder_path = './papers/'
+	pdf_files_list = get_pdfs_in_subfolder(subfolder_path)
+	numPDFs = len(pdf_files_list)
+
+	print("PDF Files in Subfolder:")
+	count = 1
+	for pdf_file in pdf_files_list:
+		print(count,'/',numPDFs, pdf_file)
+		generate_thumb(pdf_file)
+		count+=1
+
+if __name__ == "__main__":
+	print("Running...")
+	generate_thumbs()
+	print("Complete.")
